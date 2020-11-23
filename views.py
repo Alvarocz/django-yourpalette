7a6a36d40f13c506e14af33160340264acd8f00f
Alvarocz <alvaroczxp@gmail.com>
2020-11-23 16:49:57 -0500

    fixed kwargs

--- views.py
+++ views.py
@@ -152,7 +152,7 @@ class ColorPaletteCreateView(YourPaletteMixin, CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        print(context)
+        print(context['form'].fields['dark'])
         context['action'] = 'new'
         return context
 
